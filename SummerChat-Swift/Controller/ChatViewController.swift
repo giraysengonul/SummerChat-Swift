a61574678a86a8d997306524b4ce0b83fe121b04
hakkicansengonul <coder.9734@gmail.com>
2022-08-19 23:00:58 +0300

    ControllerViews are arranged

--- SummerChat-Swift/Controller/ChatViewController.swift
+++ SummerChat-Swift/Controller/ChatViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ChatViewController: UIViewController {
-
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +19,7 @@ class ChatViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func sendButton(_ sender: UIButton) {
     }
-    */
-
 }
